ea5e9c2d0b7e158580f0fdf851b9ef15ae33c37c
varshaeturi <varshauoh@gmail.com>
2022-02-09 14:14:45 +0530

    Add files via upload

---
+++ Hello_World.py
@@ -0,0 +1,9 @@
+# A simple hello, world 
+
+print ('Hello, world!')
+print ('What is your name?') #asks for name
+myName = input()
+print ('It is good to meet you,' + myName)
+print ('what is your age?') #asks for your age
+myAge = input()
+print ('You will be'  + str(int(myAge) + 1) + 'in a year. Cool!')
